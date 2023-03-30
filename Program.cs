db08c3d0179ae4e674ddb2ac2401cce5fb51be80
Emiliano23atr <joaovitoremiliano5@gmail.com>
2023-03-30 20:39:36 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// using ExerciciosCsharp.Exercicios;
+
+namespace ExerciciosCsharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // {"Primeiro Programa", Exercicio001.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
